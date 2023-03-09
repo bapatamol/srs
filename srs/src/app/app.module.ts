68e6c885c5f867540b44b930e98f597e2e88bb94
A <B>
2023-03-08 20:33:02 -0500

    css updates and clean up

--- srs/src/app/app.module.ts
+++ srs/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
