464afa4fdf39198c7561342ebb2b4d99a216be1c
A <B>
2023-03-14 19:32:54 -0400

    adding ngrx fixing node on local

---
+++ srs/src/app/learn/learn.reducer.ts
@@ -0,0 +1,12 @@
+import { Action } from "@ngrx/store";
+
+const initialState = {
+    cards : []
+};
+
+// export function learnReducer(state - initialState, action: Action) {
+//     switch(action.type) {
+
+//     }
+
+// }
